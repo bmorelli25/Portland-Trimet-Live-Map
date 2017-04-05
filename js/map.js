ce9f5906348535cf3cbf93d7ed087a0b33ac2355
Brandon Morelli <bmorelli25@gmail.com>
2017-04-05 16:33:28 -0700

    server cleans up data and passes to front end

--- js/map.js
+++ js/map.js
@@ -14,126 +14,14 @@ var mapOptions = {
 //creates the map in the div"map" using the map options
 map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-//get JSON from Trimet.com
-$.getJSON('http://developer.trimet.org/ws/v2/vehicles?APPID=155EA63E56014EC522C98433B', function(trimetData) {
-let vehicleData = trimetData.resultSet.vehicle;
-
-  //Create Vehicle Class
-  let Vehicle = function(bus){
-    this.longitude = bus.longitude;
-    this.latitude = bus.latitude;
-    this.routeNum = bus.routeNumber;
-    this.title = bus.signMessageLong;
-    this.direction = bus.direction; //busDirection
-    this.id = bus.vehicleID; //vehicleID
-    this.delay = bus.delay; //delayV
-    this.type = bus.type; // rail or bus
-    this.marker = null; //marker placeholder
-  };
-
-  Vehicle.prototype = {
-    createLatLng: function(){ //latlngTemp
-      return new google.maps.LatLng(this.latitude, this.longitude);
-    },
-    fillColor: function(){
-      switch (this.routeNum){
-        case 90:
-          //red
-          return this.direction ? '#F44336' : '#F44336';
-        case 100:
-          //blue
-          return this.direction ? '#2196F3' : '#2196F3';
-        case 190:
-          //yellow
-          return this.direction ? '#FFEB3B' : '#FFEB3B';
-        case 200:
-          //green
-          return this.direction ? '#4CAF50' : '#4CAF50';
-        case 290:
-          //orange
-          return this.direction ? '#FF9800' : '#FF9800';
-        default:
-          //bus
-          return this.direction ? '#607D8B' : '#607D8B';
-        }
-      },
-      createIcon: function(){
-        return {
-          path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
-          fillColor: this.fillColor(),
-          fillOpacity: 0.6,
-          strokeColor: this.direction ? '#ffffff' : '#000000' ,
-          strokeWeight: 2,
-          strokeOpacity: 0.6,
-          scale: 0.5,
-          text: "57"
-        };
-      },
-      createMarker: function(){
-        this.marker = new google.maps.Marker({
-          position: this.createLatLng(),
-          map: map,
-          icon: this.createIcon(),
-          title: this.title,
-          busNum: this.routeNum,
-          uniqueID: this.id
-          //title: titleTemp
-        });
-
-        let content = (
-          '<div class="popup_container">' +
-            this.title +
-            '<br>' +
-            ( this.delay > 0 ? 'Ahead of schedule by ' : 'Behind schedule by ') +
-            Math.abs(this.delay) +
-            ' seconds. </div>'
-        );
-
-        let infowindow = new google.maps.InfoWindow({
-          content,
-          maxWidth: 300
-        });
-
-        this.marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-      }
-    };
+$.getJSON('http://localhost:3000', function(trimetData) {
+let vehicleData = trimetData;
 
   vehicleData.forEach(function(vd){
-    if(vd.vehicleID === null) return;
-
-    let bus = new Vehicle(vd);
-    busArray.push(bus);
-    bus.createMarker();
+    createMarker(vd);
+  //busArray.push(vd);
   });
-
-  console.log(busArray);
 });
-
-setInterval(function(){
-  $.getJSON('http://developer.trimet.org/ws/v2/vehicles?APPID=155EA63E56014EC522C98433B', function(trimetData) {
-    let vehicleData = trimetData.resultSet.vehicle;
-
-    busArray.forEach(function(bus,i){
-      vehicleData.forEach(function(newBus){
-        if(newBus.vehicleID === bus.id){
-          if(newBus.longitude !== bus.longitude || newBus.latitude !== bus.latitude){
-            busArray[i].longitude = newBus.longitude;
-            busArray[i].latitude = newBus.latitude;
-            busArray[i].routeNum = newBus.routeNumber;
-            busArray[i].title = newBus.signMessageLong;
-            busArray[i].direction = newBus.direction; //busDirection
-            busArray[i].id = newBus.vehicleID; //vehicleID
-            busArray[i].delay = newBus.delay; //delayV
-            busArray[i].type = newBus.type; // rail or bus
-            busArray[i].marker.setPosition(busArray[i].createLatLng());
-          }
-        }
-      });
-    });
-  });
-}, 5000);
 
 //still need to refactor
 $(window).resize(function () {
@@ -142,3 +30,63 @@ $(window).resize(function () {
 
     $('#map').css('height', h-offsetTop);
 }).resize();
+
+function createMarker(bus){
+  let marker = new google.maps.Marker({
+    position: function(){ //latlngTemp
+      return new google.maps.LatLng(bus.latitude, bus.longitude);
+    }(),
+    map: map,
+    icon: function(){
+      return {
+        path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
+        fillColor: fillColor(bus.routeNum, bus.direction),
+        fillOpacity: 0.6,
+        strokeColor: bus.direction ? '#ffffff' : '#000000' ,
+        strokeWeight: 2,
+        strokeOpacity: 0.6,
+        scale: 0.5,
+        text: "57"
+      };
+    }(),
+    title: bus.title,
+    busNum: bus.routeNum,
+    uniqueID: bus.id
+    //title: titleTemp
+  });
+
+  let content = (
+    '<div class="popup_container">' +
+      bus.title +
+      '<br>' +
+      ( bus.delay > 0 ? 'Ahead of schedule by ' : 'Behind schedule by ') +
+      Math.abs(bus.delay) +
+      ' seconds. </div>'
+  );
+
+  let infowindow = new google.maps.InfoWindow({
+    content,
+    maxWidth: 300
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+function fillColor(routeNum,direction){
+  switch (routeNum){
+    case 90://red
+      return direction ? '#F44336' : '#F44336';
+    case 100://blue
+      return direction ? '#2196F3' : '#2196F3';
+    case 190://yellow
+      return direction ? '#FFEB3B' : '#FFEB3B';
+    case 200://green
+      return direction ? '#4CAF50' : '#4CAF50';
+    case 290://orange
+      return direction ? '#FF9800' : '#FF9800';
+    default://bus
+      return direction ? '#607D8B' : '#607D8B';
+  }
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     request = require('request');
 
-var ProcessData = require('./ProcessData');
+var busArray = [];
 
 var app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,17 +10,42 @@ const URL = 'http://developer.trimet.org/ws/v2/vehicles?APPID=155EA63E56014EC522
 //tell it which folder we want to serve
 app.use(express.static('public'));
 
+//CORS middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 request(URL, function(err, res, body){
   if(err){
     console.log('Error fetching data from Trimet: ',err);
   } else {
-    let vehicleData = JSON.parse(body).resultSet.vehicle;
-    let vehicles = ProcessData.go(vehicleData);
+    let vehicles = JSON.parse(body).resultSet.vehicle;
+
+    let Vehicle = function(bus){
+      this.longitude = bus.longitude;
+      this.latitude = bus.latitude;
+      this.routeNum = bus.routeNumber;
+      this.title = bus.signMessageLong;
+      this.direction = bus.direction; //busDirection
+      this.id = bus.vehicleID; //vehicleID
+      this.delay = bus.delay; //delayV
+      this.type = bus.type; // rail or bus
+    };
+
+    vehicles.forEach(function(vehicle){
+      if(vehicle.nextStopSeq === null) return;
+
+      let bus = new Vehicle(vehicle);
+      busArray.push(bus);
+
+    });
   }
 });
 
 app.get('/', function(req,res){
-  res.send('It works');
+  res.send(busArray);
 });
 
 //start the server
